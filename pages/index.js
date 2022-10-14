1fa2f3e79c378ac1e19c99cc4e7e6c51dd344926
KaldiHQ <115192860+KaldiHQ@users.noreply.github.com>
2022-10-13 23:37:28 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
+import React from "react";
+import { Button } from "reactstrap";
+function Example() {
+    return (
+      <>
+        <Button className="btn-icon-only" color="twitter" type="button">
+        {" "}
+        <span className="btn-inner--icon">
+          <i className="fab fa-twitter"></i>
+        </span>
+      </Button>
+      <Button className="btn-icon-only" color="instagram" type="button">
+        {" "}
+        <span className="btn-inner--icon">
+          <i className="fab fa-instagram"></i>
+        </span>
+      </Button>
+     </>
+    );
+}
+export default Example;
 export default function Home() {
   return (
     <div className="container">
@@ -17,11 +37,6 @@ export default function Home() {
           Future home of KaldiMarket - a specialty coffee trading platform creating powerful solutions for smallholder farmers
         </p>
       </main>
-    
-      <body>
-        <p><a class="btn btn-primary" style="background-color: #55acee;" href="https://twitter.com/KaldiMarket/" role="button"><i class="fab fa-twitter me-2"></i>Twitter</a>
-        </p>
-      </body>
     
       <Footer />
     </div>
