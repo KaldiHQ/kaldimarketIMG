a2f654bc830aa92184e64675dfaede8b5c9fabc3
KaldiHQ <115192860+KaldiHQ@users.noreply.github.com>
2022-10-13 22:47:57 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,6 @@ export default function Home() {
         <p className="description">
           Future home of KaldiMarket - a specialty coffee trading platform creating powerful solutions for smallholder farmers
         </p>
-        <div className="flex-container">
-         <div><a href="https://www.instagram.com/kaldimarket/" class="fa fa-instagram"></a></div>
-         <div><a href="https://twitter.com/KaldiMarket/" class="fa fa-twitter"></a></div>
-        </div>
       </main>
 
       <Footer />
