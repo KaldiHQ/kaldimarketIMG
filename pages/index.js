f91d590230784d3451e092ce02d5097c466eb910
KaldiHQ <115192860+KaldiHQ@users.noreply.github.com>
2022-11-10 13:13:11 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,15 @@ export default function Home() {
 
       <main>
         <p><img src="/logo.png" alt="KaldiMarket Logo" width="500" /></p>
-        <Header title="Coming Soon" />
-        <p className="description">
-          KaldiMarket™ is the world’s first direct-to-market green specialty coffee volume trading
-          platform. The Kaldi mission is to revolutionize the green specialty coffee market via a value-
-          creating coffee ecosystem; utilizing blockchain technology to help lift millions of smallholder
-          farmers out of poverty, and allow them to financially participate in a completely new
-          economic model in the global coffee sector.
-        </p>
+        <div class="container">
+          <p className="description">
+            KaldiMarket™ is the world’s first direct-to-market green specialty coffee volume trading
+            platform. The Kaldi mission is to revolutionize the green specialty coffee market via a value-
+            creating coffee ecosystem; utilizing blockchain technology to help lift millions of smallholder
+            farmers out of poverty, and allow them to financially participate in a completely new
+            economic model in the global coffee sector.
+         </p>
+        </div>
       </main>
     
       <Footer />
