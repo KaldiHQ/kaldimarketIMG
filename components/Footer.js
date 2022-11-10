cae3e34a51ed99e398fdf64665bdc61a8c9c6b28
KaldiHQ <115192860+KaldiHQ@users.noreply.github.com>
2022-11-10 12:56:17 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,8 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Made with <img src="/logoicon.png" alt="KaldiMarket Logo" className={styles.logo} />
+        <p> | “KaldiMarket” is a registered trademark of KaldiMarket, Inc. Copyright © 2022 KaldiMarket, Inc. All Rights Reserved.
+        </p>
       </footer>
     </>
   )
