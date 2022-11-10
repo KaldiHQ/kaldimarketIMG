545f90ca79e746f4026cf8d87e6337bdc29ea0d3
KaldiHQ <115192860+KaldiHQ@users.noreply.github.com>
2022-11-10 14:17:17 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className="flex-container">
-          <p><img src="/logo.png" alt="KaldiMarket Logo" width="500" /></p>
-        </div>
+          <p className="flex-container"><img src="/logo.png" alt="KaldiMarket Logo" width="500" /></p>
         <div class="col-md-8">
           <p className="description">
             KaldiMarket™ is the world’s first direct-to-market green specialty coffee volume trading
